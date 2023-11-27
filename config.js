2a3fbdbbfb693e0b995fd2df363f0ead29b0e721
Aviral <124311066+AnonO6@users.noreply.github.com>
2023-11-27 13:37:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
 // Coneection to your firebase data goes here
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAXbe9_OgKX3OgY3YZlwNUPPADk3aJ_GUM',
-  authDomain: 'busbookinglnmiit.firebaseapp.com',
-  databaseURL:
-    'https://busbookinglnmiit-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'busbookinglnmiit',
-  storageBucket: 'busbookinglnmiit.appspot.com',
-  messagingSenderId: '95672195668',
-  appId: '1:95672195668:web:85c6fe4603a9eb2e722bc1',
-  measurementId: 'G-7DK53SF97F',
+
 };
