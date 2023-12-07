3e1981ae2e014d8969ab10a8c12416f0e6059e1d
aviral shukla <aviralshukla@Mani.local>
2023-12-07 08:07:25 +0530

    COMING SOON FOR OPERATOR PAGE

--- Screen/Login/OperatorLogin.js
+++ Screen/Login/OperatorLogin.js
@@ -1,6 +1,26 @@
-import React from 'react';
 import Scan from './Operator/scan';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
 function OperatorLogin() {
-  //   return <Scan />;
+  // return <Scan />;
+  return (
+    <View>
+      <Text style={styles.titleText}>COMING SOON</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 40,
+    backgroundColor: '#9FA8DA',
+    minHeight: '100%',
+    maxHeight: '100%',
+  },
+});
+
 export default OperatorLogin;
